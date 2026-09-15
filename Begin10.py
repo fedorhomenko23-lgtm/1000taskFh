6d693dd54ed88030b43b2af4de30150542f7c3c3
fedorhomenko23-lgtm <fedorhomenko23@gmail.com>
2026-09-16 01:31:34 +0300

    Add files via upload

---
+++ Begin10.py
@@ -0,0 +1,10 @@
+a = int(input('введите число a:'))
+b = int(input('введите число b:'))
+summa = a ** 2 + b ** 2
+rasn = a ** 2 - b ** 2
+proiz = a ** 2 * b ** 2
+chast = a ** 2 / b ** 2
+print(f'Сумма равна: {summa}')
+print(f'Разность равна: {rasn}')
+print(f'Произведение равно: {proiz}')
+print(f'Частное равно: {chast}')
